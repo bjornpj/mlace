35a9508eebda7bc83f223f92762cee4fcd320edc
bjornpj <88040464+bjornpj@users.noreply.github.com>
2025-03-01 21:19:50 -0500

    Delete src directory

--- src/utils/logger.py
+++
@@ -1,8 +0,0 @@
-## src/utils/logger.py
-import datetime
-from colorama import Fore, Style
-
-def log_message(agent_name, message, color, level="INFO"):
-    """Utility function to log messages with a timestamp and hierarchical structure."""
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"{color}[{timestamp}] [{agent_name}] [{level}] {message}")
